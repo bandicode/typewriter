f2993d6680a280fdd1c1ba002841cec7aa4f4b07
bandicode <vincent.chambrin@gmail.com>
2021-02-28 11:32:42 +0100

    update copyright year

--- qt/src/widget/codeeditor-widget.cpp
+++ qt/src/widget/codeeditor-widget.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Vincent Chambrin
+// Copyright (C) 2020-2021 Vincent Chambrin
 // This file is part of the typewriter library
 // For conditions of distribution and use, see copyright notice in LICENSE
 
@@ -12,7 +12,6 @@
 #include <QPaintEvent>
 
 #include <QPainter>
-#include <QPainterPath>
 
 #include <QScrollBar>
 
